ad876abe26a6bbb34aa19f97fb45e00d9050434a
sayanar <33630255+sayanar@users.noreply.github.com>
2018-05-30 16:26:43 +0300

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* read specified number of 256-byte records from socket
+and validate them */
 int prompt_and_save(char *question, unsigned char *data, int len) {
 unsigned char filename[32];
 int fd;
@@ -11,4 +24,14 @@ fd = open(filename, O_WRONLY | O_CREAT, 0666);
 write(fd, data, len);
 close(fd);
 return 1;
+}
+
+
+int main ()
+{
+    char prompt[] = "test";
+    int j =5;
+    int i = prompt_and_save ("test","123",10	);
+    printf ("%d", i);
+    return 0;
 }
