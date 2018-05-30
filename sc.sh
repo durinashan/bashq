ff9bf080da72b33e1524c001389818aaa2d71cd4
sayanar <33630255+sayanar@users.noreply.github.com>
2018-05-30 23:54:43 +0300

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
-# remove files with name pattern matching regular expression
-
-echo -n "Enter directories: "
-read dirs
-
-echo -n "Enter filename regular expression: "
-read pattern
-
-find $dirs > /tmp/result
-#cat /tmp/result
-cmd='rm -i  `grep '$pattern' /tmp/result`'
-echo "Running command $cmd"
-eval $cmd
-
-rm /tmp/result
+# remove files with name pattern matching regexp
+[ x$1 = x ] && { echo -n "Enter directory: "; read dir } || dir=$1
+[ x$2 = x ] && { echo -n "Enter pattern: "; read pat } || pat=$1
+find $dir > /tmp/listing
+cmd=’rm ‘grep ’$pat’ /tmp/listing‘’
+echo "Running command $cmd"; eval $cmd
